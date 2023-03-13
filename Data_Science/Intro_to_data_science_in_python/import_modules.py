9398ee087019ea4e7ece679a60a1720e8a602660
Sedoo Bitto <sedoobitto@gmail.com>
2023-03-13 17:30:31 +0100

    import modules

---
+++ Data_Science/Intro_to_data_science_in_python/import_modules.py
@@ -0,0 +1,8 @@
+# Use an import statement to import statsmodels
+import statsmodels
+
+# import statsmodels under the alias sm
+import statsmodels as sm
+
+# import seaborn with alias sns
+import seaborn as sns
